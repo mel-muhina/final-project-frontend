57cc0e9cf99c1ccfc819ee197aec34499ee3da38
mel-muhina <mel.muhina@gmail.com>
2024-08-27 11:50:54 +0100

    Add trial logos to asset and file structure.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+// import logo1 from './assets/logo1.png'
+// import logo2 from '../public/favicon2.png'
+import logo2 from '../public/favicon11.png'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={logo2} />
+        <h1>Homepage</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
