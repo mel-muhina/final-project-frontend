3eb22cebc0e593e7b7516cc72c123960a9bbb72d
mel-muhina <mel.muhina@gmail.com>
2024-08-29 22:51:22 +0100

    Add chosen tags, new icons and hamburger menu.

--- src/components/TagCard/index.jsx
+++ src/components/TagCard/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import './TagCard.css'
+import tagNotification from '../../assets/images/noti.png'
 
 export default function TagCard() {
 
@@ -15,22 +16,25 @@ export default function TagCard() {
         const dummyTags = [
         {
             id: 1,
-            tag: "Nature",
+            tag: "Woodlands",
         }, {
             id: 2,
             tag: "Hiking",
-
         }, {
             id: 3, 
-            tag: "Active"
-
+            tag: "Beach"
         }, {
             id: 4, 
-            tag: "Things To Do"
-        }       
+            tag: "Camping"
+        }, {
+            id: 5, 
+            tag: "Park"
+        },  {
+            id: 6, 
+            tag: "Garden"
+        }             
         ]
     
-
     async function populateTags() {
         // const api = `http://54.89.47.53:3000/locations/images/${randomId}`
         // const response = await fetch(api);
@@ -44,12 +48,15 @@ export default function TagCard() {
   return (
     <>  
         <div className="tags-container">
-                    <h2>Location Tags</h2>
+                    <div className='tags-title'>
+                        <h2>Location Tags</h2>
+                        <img src={tagNotification} className='tags-notification'></img>   
+                    </div>
                 <ul> 
                         {tags.map(tag => 
                           <li className={tag.tag}> <Link to="">{tag.tag}</Link></li>
                         )}
-                    
+                     
                 </ul>
         </div>
     </>
